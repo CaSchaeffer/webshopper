1bd651997adfa6f267d4818ffa2435f83528472b
Carl <125189035+CaSchaeffer@users.noreply.github.com>
2023-09-10 15:14:07 +0200

    script.js
    Added the Shopping Cart and Item Quantity to the Javascript to it reacts to choosen items

--- script.js
+++ script.js
@@ -24,16 +24,32 @@ let shoppingCart = [];
 let totalPrice = 0;
 
 // Function to add an item to the shopping cart
-function addToCart(itemName, itemPrice) {
-    // Add the item to the shopping cart array
-    shoppingCart.push({ name: itemName, price: itemPrice });
+function addToCart(itemName, itemPrice, quantityInputId) {
+    // Get the quantity input element associated with the item
+    const quantityInput = document.getElementById(quantityInputId);
 
-    // Update the shopping cart list in the HTML
-    updateCartList();
+    // Get the quantity value from the input
+    const quantity = parseInt(quantityInput.value);
 
-    // Update the total price and display it
-    totalPrice += itemPrice;
-    updateTotalPrice();
+    if (quantity > 0) {
+        // Check if the item is already in the cart
+        const existingItemIndex = shoppingCart.findIndex(item => item.name === itemName);
+
+        if (existingItemIndex !== -1) {
+            // If the item exists in the cart, update its quantity
+            shoppingCart[existingItemIndex].quantity += quantity;
+        } else {
+            // If the item is not in the cart, add it
+            shoppingCart.push({ name: itemName, price: itemPrice, quantity: quantity });
+        }
+
+        // Update the shopping cart list in the HTML
+        updateCartList();
+
+        // Update the total price and display it
+        totalPrice += itemPrice * quantity;
+        updateTotalPrice();
+    }
 }
 
 // Function to update the shopping cart list in the HTML
@@ -44,7 +60,7 @@ function updateCartList() {
     // Iterate through the shopping cart items and create list items
     shoppingCart.forEach(item => {
         const listItem = document.createElement('li');
-        listItem.textContent = `${item.name} - ${item.price.toFixed(2)} €`;
+        listItem.textContent = `${item.name} - Price: ${item.price.toFixed(2)} € - Quantity: ${item.quantity}`;
         cartList.appendChild(listItem);
     });
 }
@@ -53,4 +69,22 @@ function updateCartList() {
 function updateTotalPrice() {
     const totalElement = document.querySelector('.shopping-cart p');
     totalElement.textContent = `Gesamt: ${totalPrice.toFixed(2)} €`;
+}
+
+// Function to clear the shopping cart
+function clearCart() {
+    shoppingCart = []; // Empty the shopping cart array
+    totalPrice = 0; // Reset the total price to zero
+    updateCartList(); // Update the shopping cart list in the HTML
+    updateTotalPrice(); // Update the total price in the HTML
+}
+
+// Function to simulate the checkout process (you can replace this with your actual checkout logic)
+function checkout() {
+    if (shoppingCart.length > 0) {
+        alert('Checkout Successful!'); // Replace with your checkout logic
+        clearCart(); // Clear the cart after checkout
+    } else {
+        alert('The shopping cart is empty.'); // Display a message if the cart is empty
+    }
 }
